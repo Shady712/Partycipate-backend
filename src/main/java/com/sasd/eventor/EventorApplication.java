183347c818d9d98497d9769ffd83c3c0e08f78d4
Shady712 <frostmorn712@gmail.com>
2022-01-05 23:01:41 +0300

    initial commit (dependencies + db config)

---
+++ src/main/java/com/sasd/eventor/EventorApplication.java
@@ -0,0 +1,13 @@
+package com.sasd.eventor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventorApplication.class, args);
+	}
+
+}
