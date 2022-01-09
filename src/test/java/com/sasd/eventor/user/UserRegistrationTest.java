4579920bd29ed0b5e16eeb9c84e49955a178de79
Shady712 <frostmorn712@gmail.com>
2022-01-10 00:16:02 +0300

    exception used login tests implemented

--- src/test/java/com/sasd/eventor/user/UserRegistrationTest.java
+++ src/test/java/com/sasd/eventor/user/UserRegistrationTest.java
@@ -1,8 +1,11 @@
 package com.sasd.eventor.user;
 
 import com.sasd.eventor.controllers.UserController;
+import com.sasd.eventor.exception.EventorException;
 import com.sasd.eventor.model.daos.UserRepository;
 import com.sasd.eventor.model.dtos.UserRegisterDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +20,11 @@ public class UserRegistrationTest {
     private static final String VALID_LOGIN = "Valid12345";
     private static final String VALID_NAME = "Valid Name";
     private static final String VALID_PASSWORD = "QWErty12345";
+
+    @BeforeEach
+    public void init() {
+        userRepository.deleteAll();
+    }
 
     @Test
     public void registerValidUser() {
@@ -36,7 +44,13 @@ public class UserRegistrationTest {
 
     @Test
     public void ensureBadRequestForUsedLogin() {
-        // TODO
+        var userRegisterDto = validUserRegisterDto();
+        userController.register(userRegisterDto);
+        userRegisterDto.setName(VALID_NAME + VALID_NAME);
+        userRegisterDto.setPassword(VALID_PASSWORD + VALID_PASSWORD);
+        Assertions.assertThrows(EventorException.class, () ->
+                userController.register(userRegisterDto)
+        );
     }
 
     private UserRegisterDto validUserRegisterDto() {
