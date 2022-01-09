cc48886d0a904b0ec3f2911be30eec23692d1f40
Shady712 <frostmorn712@gmail.com>
2022-01-09 13:11:45 +0300

    user-validation-base

--- src/main/java/com/sasd/eventor/services/converters/UserCredentialsDtoToUserEntityConverter.java
+++ src/main/java/com/sasd/eventor/services/converters/UserCredentialsDtoToUserEntityConverter.java
@@ -1,15 +1,15 @@
 package com.sasd.eventor.services.converters;
 
-import com.sasd.eventor.model.dtos.UserCredentialsDto;
+import com.sasd.eventor.model.dtos.UserRegisterDto;
 import com.sasd.eventor.model.entities.User;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserCredentialsDtoToUserEntityConverter implements Converter<UserCredentialsDto, User> {
+public class UserCredentialsDtoToUserEntityConverter implements Converter<UserRegisterDto, User> {
 
     @Override
-    public User convert(UserCredentialsDto source) {
+    public User convert(UserRegisterDto source) {
         var record = new User();
         record.setLogin(source.getLogin());
         record.setName(source.getName());
