c6a4515644cd0b53e8d599ba01f021305af4562c
Антон <supertonic2002@gmail.com>
2022-01-12 23:01:56 +0300

    user-reg-fix changed class of register method (now it's User instead of void); added return of registered user inside it.

--- src/main/java/com/sasd/eventor/controllers/UserController.java
+++ src/main/java/com/sasd/eventor/controllers/UserController.java
@@ -17,11 +17,12 @@ public class UserController {
     private final ConversionService conversionService;
 
     @PostMapping("/register")
-    public void register(@RequestBody @Valid UserRegisterDto userRegisterDto) {
+    public User register(@RequestBody @Valid UserRegisterDto userRegisterDto) {
         if (!userService.checkLoginVacancy(userRegisterDto.getLogin())) {
             throw new EventorException("Provided login is already in use");
         }
         userService.register(conversionService.convert(userRegisterDto, User.class));
+        return conversionService.convert(userRegisterDto, User.class);
     }
 
     @GetMapping("/findById")
