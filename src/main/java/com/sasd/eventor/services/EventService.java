1d1c6cf717a6f03baa9335dc56193a7903d7efe4
Антон <supertonic2002@gmail.com>
2022-01-13 20:35:35 +0300

    events-by-creator now you can find all events by login of their creator

--- src/main/java/com/sasd/eventor/services/EventService.java
+++ src/main/java/com/sasd/eventor/services/EventService.java
@@ -5,12 +5,14 @@ import com.sasd.eventor.model.entities.Event;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
+    private final UserService userService;
 
     public Optional<Event> findById(Long id) {
         return eventRepository.findById(id);
@@ -18,5 +20,9 @@ public class EventService {
 
     public Event createEvent(Event event) {
         return eventRepository.save(event);
+    }
+
+    public List<Event> findAllByCreator(String login) {
+        return eventRepository.findAllByCreator(userService.findByLogin(login));
     }
 }
