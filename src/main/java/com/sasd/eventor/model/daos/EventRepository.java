0a6e0f8bb3ca46e9bf24c66c3020cf24136d9c68
Антон <supertonic2002@gmail.com>
2022-01-18 17:11:27 +0300

    events-by-creator added not-working tests

--- src/main/java/com/sasd/eventor/model/daos/EventRepository.java
+++ src/main/java/com/sasd/eventor/model/daos/EventRepository.java
@@ -1,13 +1,11 @@
 package com.sasd.eventor.model.daos;
 
-import com.sasd.eventor.model.dtos.UserResponseDto;
 import com.sasd.eventor.model.entities.Event;
 import com.sasd.eventor.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
--- src/test/java/com/sasd/eventor/event/EventTest.java
+++ src/test/java/com/sasd/eventor/event/EventTest.java
@@ -31,11 +31,21 @@ public abstract class EventTest {
 
     protected static EventCreateDto validEventCreateDtoWithoutJwt() {
         var dto = new EventCreateDto();
-        dto.setName(VALID_NAME);
+       dto.setName(VALID_NAME);
         dto.setDate(VALID_DATE);
         dto.setDescription(VALID_DESCRIPTION);
         dto.setLocation(VALID_LOCATION);
         dto.setPrice(VALID_PRICE);
+        return dto;
+    }
+
+    protected static EventCreateDto validEventCreateDtoWithoutJwtSnd() {
+        var dto = new EventCreateDto();
+        dto.setName(VALID_NAME+'1');
+        dto.setDate(VALID_DATE);
+        dto.setDescription(VALID_DESCRIPTION+'1');
+        dto.setLocation(VALID_LOCATION+'1');
+        dto.setPrice(VALID_PRICE+1);
         return dto;
     }
 
@@ -44,6 +54,15 @@ public abstract class EventTest {
         dto.setLogin(VALID_USER_LOGIN);
         dto.setName(VALID_USER_NAME);
         dto.setPassword(VALID_USER_PASSWORD);
+        userController.register(dto);
+        return userController.createJwt(dto.getLogin(), dto.getPassword());
+    }
+
+    protected String validJwtSnd() {
+        var dto = new UserRegisterDto();
+        dto.setLogin(VALID_USER_LOGIN+"Snd");
+        dto.setName(VALID_USER_NAME+"Snd");
+        dto.setPassword(VALID_USER_PASSWORD+"Snd");
         userController.register(dto);
         return userController.createJwt(dto.getLogin(), dto.getPassword());
     }
