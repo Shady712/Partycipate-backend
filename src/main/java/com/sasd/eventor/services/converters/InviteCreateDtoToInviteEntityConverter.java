6539669e12628fefc17e0a5a3394fad20874cc68
aleks_gright <Alexander2326@yandex.ru>
2022-01-18 19:11:04 +0300

    invite-entity tested in postman, fixed

--- src/main/java/com/sasd/eventor/services/converters/InviteCreateDtoToInviteEntityConverter.java
+++ src/main/java/com/sasd/eventor/services/converters/InviteCreateDtoToInviteEntityConverter.java
@@ -6,19 +6,22 @@ import com.sasd.eventor.services.EventService;
 import com.sasd.eventor.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 @AllArgsConstructor
 public class InviteCreateDtoToInviteEntityConverter implements Converter<InviteCreateDto, Invite> {
     private final UserService userService;
     private final EventService eventService;
 
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
     @Override
     public Invite convert(InviteCreateDto source) {
-        Invite invite = new Invite();
-        invite.setEvent(eventService.findById(source.getEventId()).get());
-        invite.setReceiver(userService.findById(source.getReceiverId()).get());
-        invite.setStatus(Invite.InviteStatus.WAITING);
-        invite.setMessage(source.getMessage());
-        return invite;
+        Invite record = new Invite();
+        record.setEvent(eventService.findById(source.getEventId()).get());
+        record.setReceiver(userService.findById(source.getReceiverId()).get());
+        record.setStatus(Invite.InviteStatus.WAITING);
+        record.setMessage(source.getMessage());
+        return record;
     }
 }
