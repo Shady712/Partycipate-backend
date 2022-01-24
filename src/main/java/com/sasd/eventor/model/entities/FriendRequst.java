0a8a84f68240be9b28fc2303ccb4f38279655ee0
Shady712 <frostmorn712@gmail.com>
2022-01-24 23:47:02 +0300

    user-friends WIP

---
+++ src/main/java/com/sasd/eventor/model/entities/FriendRequst.java
@@ -0,0 +1,21 @@
+package com.sasd.eventor.model.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "friend_requests")
+public class FriendRequst {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "friend_requests_seq")
+    @SequenceGenerator(name = "friend_requests_seq", sequenceName = "friend_requests_seq")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+}
