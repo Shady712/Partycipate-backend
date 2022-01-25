1984df65474a225e0039989c9e5612e2137b16b2
Shady712 <frostmorn712@gmail.com>
2022-01-25 20:44:46 +0300

    user-friends implemented api tests done

--- src/test/java/com/sasd/eventor/friendrequest/FriendRequestFindTest.java
+++ src/test/java/com/sasd/eventor/friendrequest/FriendRequestFindTest.java
@@ -1,7 +1,11 @@
 package com.sasd.eventor.friendrequest;
 
+import com.sasd.eventor.model.dtos.FriendRequestResponseDto;
+import com.sasd.eventor.model.dtos.UserRegisterDto;
+import com.sasd.eventor.utils.UserUtils;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static com.sasd.eventor.utils.UserUtils.validUserRegisterDto;
@@ -19,16 +23,44 @@ public class FriendRequestFindTest extends FriendRequestTest {
     @Test
     public void findAllOutgoingRequests() {
         var senderDto = validUserRegisterDto();
-        var senderJwt = getJwt(senderDto);
+        abstractFindAllTest(
+                () -> senderDto,
+                UserUtils::validUserRegisterDto,
+                () -> friendRequestController.findAllOutgoing(getJwt(senderDto))
+        );
+    }
+
+    @Test
+    public void findAllIncomingRequests() {
+        var receiverDto = validUserRegisterDto();
+        abstractFindAllTest(
+                UserUtils::validUserRegisterDto,
+                () -> receiverDto,
+                () -> friendRequestController.findAllIncoming(getJwt(receiverDto))
+        );
+    }
+
+    private void abstractFindAllTest(
+            UserRegisterDtoFactory senderDtoFactory,
+            UserRegisterDtoFactory receiverDtoFactory,
+            JwtHandler jwtHandler
+    ) {
         assert Stream.of(
-                validFriendRequestCreateDto(senderDto, validUserRegisterDto()),
-                validFriendRequestCreateDto(senderDto, validUserRegisterDto()),
-                validFriendRequestCreateDto(senderDto, validUserRegisterDto())
+                validFriendRequestCreateDto(senderDtoFactory.dto(), receiverDtoFactory.dto()),
+                validFriendRequestCreateDto(senderDtoFactory.dto(), receiverDtoFactory.dto()),
+                validFriendRequestCreateDto(senderDtoFactory.dto(), receiverDtoFactory.dto())
         )
                 .map(friendRequestCreateDto -> friendRequestController.createRequest(friendRequestCreateDto))
                 .toList()
-                .equals(friendRequestController.findAllOutgoing(senderJwt));
-        assert !friendRequestController.findAllOutgoing(senderJwt)
-                .contains(friendRequestController.createRequest(validFriendRequestCreateDto()));
+                .equals(jwtHandler.handleJwt());
+        assert !jwtHandler.handleJwt().contains(friendRequestController.createRequest(validFriendRequestCreateDto()));
+    }
+
+    private interface UserRegisterDtoFactory {
+        UserRegisterDto dto();
+    }
+
+    private interface JwtHandler {
+        List<FriendRequestResponseDto> handleJwt();
     }
 }
