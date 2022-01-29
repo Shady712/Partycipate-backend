88a8f2a6aba89b11688d8f69ff9cd391b4c5ff43
Антон <supertonic2002@gmail.com>
2022-01-29 15:03:44 +0300

    invite
    InviteResponseDto now contains userResponseDto and eventResponse dto,
    nor userLogin and eventId
    
    fixed small issues

--- src/main/java/com/sasd/eventor/services/converters/InviteEntityToInviteResponseDtoConverter.java
+++ src/main/java/com/sasd/eventor/services/converters/InviteEntityToInviteResponseDtoConverter.java
@@ -2,18 +2,25 @@ package com.sasd.eventor.services.converters;
 
 import com.sasd.eventor.model.dtos.InviteResponseDto;
 import com.sasd.eventor.model.entities.Invite;
+import lombok.AllArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class InviteEntityToInviteResponseDtoConverter implements Converter<Invite, InviteResponseDto> {
-
+    private final UserEntityToUserResponseDtoConverter userEntityToUserResponseDtoConverter;
+    private final EventEntityToEventResponseDtoConverter eventEntityToEventResponseDtoConverter;
     @Override
     public InviteResponseDto convert(Invite source) {
         var record = new InviteResponseDto();
         record.setId(source.getId());
-        record.setReceiverLogin(source.getReceiver().getLogin());
-        record.setEventId(source.getEvent().getId());
+        record.setReceiver(
+                userEntityToUserResponseDtoConverter.convert(source.getReceiver())
+        );
+        record.setEvent(
+                eventEntityToEventResponseDtoConverter.convert(source.getEvent())
+        );
         record.setMessage(source.getMessage());
         record.setStatus(source.getStatus());
         return record;
