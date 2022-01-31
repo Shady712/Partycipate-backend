67b45bd1b3b357d78080ed93f2a77d9139877b59
Shady712 <frostmorn712@gmail.com>
2022-01-31 23:07:37 +0300

    logging-services added logging on service level

--- src/main/java/com/sasd/eventor/services/InviteService.java
+++ src/main/java/com/sasd/eventor/services/InviteService.java
@@ -6,6 +6,8 @@ import com.sasd.eventor.model.entities.Invite;
 import com.sasd.eventor.model.entities.User;
 import com.sasd.eventor.services.utils.MailService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,12 +18,18 @@ import java.util.Optional;
 public class InviteService {
     private final InviteRepository inviteRepository;
     private final MailService mailService;
+    private final Logger logger = LoggerFactory.getLogger(InviteService.class);
 
     public Optional<Invite> findById(Long id) {
+        logger.debug("Received request for finding invite with id '{}'", id);
         return inviteRepository.findById(id);
     }
 
     public Invite create(Invite invite) {
+        logger.info("Creating invite on event '{}', event id is '{}, sender is '{}', receiver is '{}'",
+                invite.getEvent().getName(), invite.getEvent().getId(),
+                invite.getEvent().getCreator().getLogin(), invite.getReceiver().getLogin()
+        );
         mailService.sendEmail(
                 invite.getReceiver().getEmail(),
                 "New invite!",
@@ -31,14 +39,22 @@ public class InviteService {
     }
 
     public List<Invite> findAllIncoming(User receiver) {
+        logger.debug("Received request for finding all incoming invites for receiver '{}'", receiver.getLogin());
         return inviteRepository.findAllByReceiver(receiver);
     }
 
     public List<Invite> findAllByEventId(Event event) {
+        logger.debug("Received request for finding all invites on event '{}', event id is '{}",
+                event.getName(), event.getId()
+        );
         return inviteRepository.findAllByEvent(event);
     }
 
     public void delete(Invite invite) {
+        logger.info("Deleting invite with id '{}' on event '{}', event id is '{}', sender is '{}', receiver is '{}",
+                invite.getId(), invite.getEvent().getName(), invite.getEvent().getId(),
+                invite.getEvent().getCreator().getLogin(), invite.getReceiver().getLogin()
+        );
         inviteRepository.delete(invite);
     }
 }
--- src/main/java/com/sasd/eventor/services/utils/JwtService.java
+++ src/main/java/com/sasd/eventor/services/utils/JwtService.java
@@ -8,6 +8,8 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.sasd.eventor.exception.EventorException;
 import com.sasd.eventor.model.entities.User;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC256;
@@ -19,16 +21,20 @@ public class JwtService {
 
     private final Algorithm algorithm = HMAC256(SECRET);
     private final JWTVerifier verifier = JWT.require(algorithm).build();
+    private final Logger logger = LoggerFactory.getLogger(JwtService.class);
 
     public String createJwtToken(User user) {
+        logger.info("Request for creation of jwt token for user '{}' received", user.getLogin());
         try {
             return JWT.create().withClaim("userId", user.getId()).sign(algorithm);
         } catch (JWTCreationException exception) {
+            logger.error("Jwt token creation failed for user '{}'", user.getLogin());
             throw new EventorException("JWT token creation failed due to '" + exception + "'");
         }
     }
 
     public Long decodeJwtToId(String jwt) {
+        logger.debug("Received decoding request for jwt token");
         try {
             return verifier.verify(jwt).getClaim("userId").asLong();
         } catch (JWTVerificationException exception) {
