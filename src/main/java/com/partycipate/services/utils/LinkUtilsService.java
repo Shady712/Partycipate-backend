b0e3f3e73ade128a2de8b00e65813c83db17004e
Shady712 <frostmorn712@gmail.com>
2022-02-06 17:31:49 +0300

    changed links

--- src/main/java/com/partycipate/services/utils/LinkUtilsService.java
+++ src/main/java/com/partycipate/services/utils/LinkUtilsService.java
@@ -1,11 +1,14 @@
 package com.partycipate.services.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LinkUtilsService {
-    public static final String EMAIL_VERIFICATION_LINK = "http://localhost:8080/api/v1/user/verifyEmail";
-    public static final String REQUEST_PASSWORD_CHANGE_LINK = "http://localhost:8080/updatePassword";
+    @Value("${email.verify.link}")
+    public static String EMAIL_VERIFICATION_LINK;
+    @Value("${password.change.link}")
+    public static String REQUEST_PASSWORD_CHANGE_LINK;
 
     public static String createLinkWithLoginAndPasswordHashAsParams(String link, String login, String passwordHash) {
         return link + substituteLoginAndPasswordHashAsRequestParams(login, passwordHash);
