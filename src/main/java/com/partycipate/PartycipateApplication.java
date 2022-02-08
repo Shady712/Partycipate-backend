e237d15501a2d89171713ae75b4aee323625602c
Aleksandr Stupnikov <super-stupnikow@mail.ru>
2022-02-09 00:08:28 +0300

    Enable cross origin requests for https://partycipate.herokuapp.com

--- src/main/java/com/partycipate/PartycipateApplication.java
+++ src/main/java/com/partycipate/PartycipateApplication.java
@@ -2,6 +2,10 @@ package com.partycipate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PartycipateApplication {
@@ -10,4 +14,13 @@ public class PartycipateApplication {
 		SpringApplication.run(PartycipateApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
+				registry.addMapping("/api/v1/**").allowedOrigins("https://partycipate.herokuapp.com");
+			}
+		};
+	}
 }
