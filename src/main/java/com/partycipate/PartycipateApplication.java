e0f3fd9e3cfdb2806b364732d8e43c4768a5e804
Shady712 <frostmorn712@gmail.com>
2022-02-11 11:58:23 +0300

    cors fixed config

--- src/main/java/com/partycipate/PartycipateApplication.java
+++ src/main/java/com/partycipate/PartycipateApplication.java
@@ -2,25 +2,11 @@ package com.partycipate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PartycipateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PartycipateApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(@NonNull CorsRegistry registry) {
-				registry.addMapping("/api/v1/**").allowedOrigins("https://partycipate.herokuapp.com");
-			}
-		};
 	}
 }
